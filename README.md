85d2ee14090070112840599d101fe35a16db4bc6
Julio1508 <jsicardi@itba.edu.ar>
2021-12-21 17:11:33 -0300

    Correccion a importacion de neo4j, cierre de conexiones correcto

--- README.md
+++ README.md
@@ -15,22 +15,14 @@ Importacion de datos a PostgreSQL con docker:
 Previamente debe iniciarse el contenedor y ejecutarse el siguiente comando para pasar los archivos al contenedor necesarios para la importacion (posicionado en el directorio donde se descargaron los datasets previos):
     docker cp .  my_postgres_container:/import_datasets
 
-Luego se debe ejecutar el script import_postgres.sql, habiendo previamente creado la base de datos adecuada. Este script puede ejecutarse mediante IDE como Datagrip u otros medios. Se detalla a continuacion el proceso usando la terminal y la herramienta psql:
-
-Abrir una terminal en el contenedor:
-    docker exec -ti my_potgres_container bash
-
-Posicionarse dentro del directorio creado con el comando:
-    cd import_datasets
-
-Para ejecutar el script usar el comando (importante dar permisos al directorio import_datasets para la generacion de csv)
-    psql -U my_user -d soccer_analyzer -a -f import_postgres.sql
+Luego desde su host local ejecutar import_postgres.js de la siguiente manera:
+    node import_postgres.ja
 
 Adicional a la importacion de los datos a la base, tambien se generan dos archivos players_history.csv y players_national_teams_history.csv, que se utilizaran a continuacion para la importacion a neo4j
 
 Importacion de datos a PostgreSQL local:
 
-Los pasos de la importacion se puede realizar con los mismos comandos de psql mencionados previamente, pero es importante que previamente se modifique el path de los archivos csv en import_postgres.sql, dado que estos solo trabajan con paths absolutos (los paths default son aquellos relacionados a la opcion con docker). En caso de poner el path absoluto hacia la carpeta /import_datasets, el script funciona de la misma forma 
+Los pasos de la importacion son los mismos, pero es importante que previamente se modifique el path de los archivos csv en import_postgres.js, modificando el path presente por el path absoluto de la carpeta import_datasets que contiene los archivos csv. En esta carpeta tambien se veran creados los csv resultantes para la importacion a Neo4j
 
 Importacion de datos a Neo4j con docker:
 
@@ -48,6 +40,6 @@ Ejecutar el script import_neo.js usando los comandos:
 
 Importacion de datos a Neo4j local:
 
-En este caso mover los archivos players_history.csv y players_national_teams_history.csv al directorio correspondiente de Neo4j y ejecutar luego los comandos:
+En este caso mover los archivos players_history.csv y players_national_teams_history.csv al directorio correspondiente de Neo4j (/import) y ejecutar luego los comandos:
     npm install
     node import_neo.js
--- import_datasets/import_neo.js
+++ import_datasets/import_neo.js
@@ -1,72 +1,85 @@
 const ApiUtils = require('../apiUtils');
 
+let neoDriver;
 let neoSession;
 
 init();
 
 function init(){
     (async() => {
-        await importToNeo().then(async() =>{
+        await importToNeo().then(() =>{
             neoSession.close();
+            neoDriver.close();
             console.log('Successfully imported to Neo4j');
         })
     })();
 }
 
 async function importToNeo(){
-    neoSession = await ApiUtils.getNeo4JConnection();
-    await neoSession
-        //delete all data present to avoid mismatch results
-        .run('MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n, r')
-        .then(async () => {
-            //create player nodes
-            console.log('Database cleaned')
-            await neoSession.run('LOAD CSV WITH HEADERS FROM \'file:///players_history.csv\' AS row MERGE (p:Player{player_id:row.player_id,name:row.player_name,nationality:row.player_nationality});')
+    neoDriver = await ApiUtils.getNeo4JConnection();
+      //delete all data present to avoid mismatch results
+    
+    neoSession = neoDriver.session();
+    await neoSession.run('MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n, r')
+    .then(async () => {
+        console.log('Database cleaned')
+         //create player nodes
+        neoSession.close();
+        neoSession = neoDriver.session();
+        await neoSession.run('LOAD CSV WITH HEADERS FROM \'file:///players_history.csv\' AS row MERGE (p:Player{player_id:row.player_id,name:row.player_name,nationality:row.player_nationality});')
             .catch((e) => {
                 console.log(`Error on creating player nodes: ${e}`);
                 neoSession.close();
                 process.exit(-1);
             });
         })
-        .then(async()=> {
-            console.log('Player nodes created')
-            //create team nodes
-            await neoSession.run('LOAD CSV WITH HEADERS FROM \'file:///players_history.csv\' AS row WITH row WHERE NOT row.team_name IS null MERGE (t:Team{team_id:row.team_id,name:row.team_name})')
-            .catch((e) => {
-                console.log(`Error on creating team nodes: ${e}`);
-                neoSession.close();
-                process.exit(-1);
-            });
-        })
-        .then(async()=> {
+    .then(async()=> {
+        console.log('Player nodes created')
+        //create team nodes
+        neoSession.close();
+        neoSession = neoDriver.session();
+        await neoSession.run('LOAD CSV WITH HEADERS FROM \'file:///players_history.csv\' AS row WITH row WHERE NOT row.team_name IS null MERGE (t:Team{team_id:row.team_id,name:row.team_name})')
+        .catch((e) => {
+            console.log(`Error on creating team nodes: ${e}`);
+            neoSession.close();
+            process.exit(-1);
+        });
+    })
+    .then(async()=> {
 
-            console.log('Team nodes created')
-            //create national team nodes
-            await neoSession.run('LOAD CSV WITH HEADERS FROM \'file:///players_national_teams_history.csv\' AS row MERGE (n:NationalTeam{national_team_id:row.national_team_id,name:row.national_team_name});')
-            .catch((e) => {
-                console.log(`Error on creating national teams nodes: ${e}`);
-                neoSession.close();
-                process.exit(-1);
-            });
-        })
-        .then(async()=>{
-            console.log('National team nodes created')
-            //create relation represents
-            await neoSession.run('LOAD CSV WITH HEADERS FROM \'file:///players_national_teams_history.csv\' AS row MATCH (p:Player{player_id:row.player_id}) MATCH (n:NationalTeam{national_team_id:row.national_team_id}) MERGE (p)-[r:represents{year:row.year}]->(n)')
-            .catch((e) => {
-                console.log(`Error on creating represent relations: ${e}`);
-                neoSession.close();
-                process.exit(-1);
-            });
-        })
-        .then(async()=>{
-            console.log('Represents relations created') 
-            //create relation played_in
-            await neoSession.run('LOAD CSV WITH HEADERS FROM \'file:///players_history.csv\' AS row WITH row WHERE NOT row.team_name IS null MATCH (p:Player{player_id:row.player_id}) MATCH (t:Team{team_id:row.team_id}) MERGE (p)-[pi:played_in{year:row.year}]->(t)')
-            .catch((e) => {
-                console.log(`Error on creating played_in relations: ${e}`);
-                neoSession.close();
-                process.exit(-1);
-            });
-        })
+        console.log('Team nodes created')
+        //create national team nodes
+        neoSession.close();
+        neoSession = neoDriver.session();
+        await neoSession.run('LOAD CSV WITH HEADERS FROM \'file:///players_national_teams_history.csv\' AS row MERGE (n:NationalTeam{national_team_id:row.national_team_id,name:row.national_team_name});')
+        .catch((e) => {
+            console.log(`Error on creating national teams nodes: ${e}`);
+            neoSession.close();
+            process.exit(-1);
+        });
+    })
+    .then(async()=>{
+        console.log('National team nodes created')
+        //create relation represents
+        neoSession.close();
+        neoSession = neoDriver.session();
+        await neoSession.run('LOAD CSV WITH HEADERS FROM \'file:///players_national_teams_history.csv\' AS row MATCH (p:Player{player_id:row.player_id}) MATCH (n:NationalTeam{national_team_id:row.national_team_id}) MERGE (p)-[r:represents{year:row.year}]->(n)')
+        .catch((e) => {
+            console.log(`Error on creating represent relations: ${e}`);
+            neoSession.close();
+            process.exit(-1);
+        });
+    })
+    .then(async()=>{
+        console.log('Represents relations created') 
+        //create relation played_in
+        neoSession.close();
+        neoSession = neoDriver.session();
+        await neoSession.run('LOAD CSV WITH HEADERS FROM \'file:///players_history.csv\' AS row WITH row WHERE NOT row.team_name IS null MATCH (p:Player{player_id:row.player_id}) MATCH (t:Team{team_id:row.team_id}) MERGE (p)-[pi:played_in{year:row.year}]->(t)')
+        .catch((e) => {
+            console.log(`Error on creating played_in relations: ${e}`);
+            neoSession.close();
+            process.exit(-1);
+        });
+    })
 }
